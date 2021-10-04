a11fdc233f2fab31dc9243bd464da1676927d737
Julia Kimball <juliakimball6@gmail.com>
2021-10-04 03:12:31 -0400

    more java
    java

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,3 +4,12 @@ import { gsap } from "gsap";
 const mainTL = gsap.timeline();
 
 
+
+mainTL.from("#hero article", {duration:2,alpha:0}) 
+
+.from("#info",{duration:2,y:"+=500", alpha:0}) 
+
+mainTL.from("#sec1",{duration:1,alpha:0}) 
+
+mainTL.from("#sec2",{duration:3,alpha:0}) 
+
