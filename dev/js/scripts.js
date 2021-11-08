2076ae6151c5395f19dc5b9d22a215293a130dac
Julia Kimball <juliakimball6@gmail.com>
2021-11-07 19:16:23 -0500

    more script
    more script

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,22 +1,37 @@
 import { gsap } from "gsap";
 
 import { GSDevTools } from "gsap/GSDevTools";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { CustomEase } from "gsap/CustomEase";
 
-gsap.registerPlugin(GSDevTools);
+
+
+gsap.registerPlugin(GSDevTools, DrawSVGPlugin, CustomEase);
 
 
 const mainTL = gsap.timeline();
 
 function First_Dots(){
+const tl = gsap.timeline({repeat: 0});
 
-    const tl = gsap.timeline();
-    tl.to("#Vector",{duration:6, scale:1, y:"-=100"});
-    return tl;
+    tl.to(".ball", {repeat: 1, duration: 1.5, ease: CustomEase.create("bounce", "M0,0 C0.126,0.382 0.282,0.496 0.44,0.644 0.632,0.824 0.818,0.001 1,0 "), y: -100,  stagger:0.5 });
+    tl.to("#Vector_3", { duration: 1, ease:  CustomEase.create("custom", "M0,0 C0.082,-0.114 0.78,1 1,1 "), y: +500 }, "same");
+    tl.to("#Vector_2", { duration: 2, ease:  CustomEase.create("custom", "M0,0 C0.082,-0.114 0.78,1 1,1 "), y: +500 }, "same");
+    tl.to("#Vector", {duration:1, ease: 1, x:"+=30"},"same");
+    tl.to("#Vector", {opacity: 0});
+    tl.from("#Glass", {duration: 1, drawSVG:"60% 60%", stagger: 0.5})
+    tl.from("#top", {duration: 1, drawSVG:"60% 60%", stagger: 0.5, fillOpacity:1 })
+    tl.from("#top", {fill:1 })
 
-    
+
+
 }
 
+
+
 mainTL.add(First_Dots());
+
+
 
 
 
