e1c302eb4d98b4de06652303d793b45825ad739c
Julia Kimball <juliakimball6@gmail.com>
2021-11-08 23:26:20 -0500

    new changes
    new changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,10 +3,13 @@ import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { CustomEase } from "gsap/CustomEase";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 
 
-gsap.registerPlugin(GSDevTools, DrawSVGPlugin, CustomEase);
+gsap.registerPlugin(GSDevTools, DrawSVGPlugin, CustomEase, MorphSVGPlugin);
+
+MorphSVGPlugin.convertToPath("Coffee_2, Vector, Vector6, top, Sauce, Cup");
 
 
 const mainTL = gsap.timeline();
@@ -14,15 +17,31 @@ const mainTL = gsap.timeline();
 function First_Dots(){
 const tl = gsap.timeline({repeat: 0});
 
-    tl.to(".ball", {repeat: 1, duration: 1.5, ease: CustomEase.create("bounce", "M0,0 C0.126,0.382 0.282,0.496 0.44,0.644 0.632,0.824 0.818,0.001 1,0 "), y: -100,  stagger:0.5 });
+    tl.to(".ball", {repeat: 1, duration: 1, ease: CustomEase.create("bounce", "M0,0 C0.126,0.382 0.282,0.496 0.44,0.644 0.632,0.824 0.818,0.001 1,0 "), y: -100,  stagger:0.5 });
     tl.to("#Vector_3", { duration: 1, ease:  CustomEase.create("custom", "M0,0 C0.082,-0.114 0.78,1 1,1 "), y: +500 }, "same");
-    tl.to("#Vector_2", { duration: 2, ease:  CustomEase.create("custom", "M0,0 C0.082,-0.114 0.78,1 1,1 "), y: +500 }, "same");
+    tl.to("#Vector_2", { duration: 1, ease:  CustomEase.create("custom", "M0,0 C0.082,-0.114 0.78,1 1,1 "), y: +500 }, "same");
     tl.to("#Vector", {duration:1, ease: 1, x:"+=30"},"same");
     tl.to("#Vector", {opacity: 0});
-    tl.from("#Glass", {duration: 1, drawSVG:"60% 60%", stagger: 0.5})
-    tl.from("#top", {duration: 1, drawSVG:"60% 60%", stagger: 0.5, fillOpacity:1 })
-    tl.from("#top", {fill:1 })
-
+    tl.from("#Glass", {duration: .75, drawSVG:"60% 60%", stagger: 0.5})
+    tl.from("#Vector5",  {duration:.75,y:"-=800", alpha:0})
+    tl.to("#Vector5", {duration: .75, morphSVG:"#Coffee_2"},);
+    tl.from("#top",  {duration:0.25,alpha:0, stagger: 0.5})
+    tl.from("#top", {duration: .75, drawSVG:"50% 65%", stagger: 0.5, fillOpacity:1 })
+    tl.to("#top", {opacity: 0},);
+    tl.from("#Vector7", {opacity: 0}, );
+    tl.to("#Vector5", {duration: .75, morphSVG:"#Glass"},);
+    tl.from("#Vector8", {opacity: 0},);
+    tl.to("#Vector5", {duration: .75, morphSVG:"#Vector8"},);
+    tl.to("#Glass", {duration: .75, morphSVG:"#Vector6"},);
+    tl.to("#Vector7", {duration:.75, ease: 1, y:"+=33"},);
+    tl.to("#Vector5", {duration: .75, morphSVG:"#Sauce"},);
+    tl.to("#Vector7", {duration: .75, morphSVG:"#Cup", y:"-=33"},);
+    tl.to("#Glass", {duration:.75, ease: 1, y:"+=33"},);
+    
+   
+    
+    
+   
 
 
 }
